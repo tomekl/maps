3c9ccc2c432a12276f42a972b06d5b6c4efcfd5d
tomekl <tomasz.lipowicz@gmail.com>
2012-10-24 16:03:35 +0100

    multi pointers and centering added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 //main.js
+p = [
+	[51.481889,-0.096609,'SandM'],
+	[51.48088471558213,-0.0850988104939,'My'],
+	[51.480264,-0.093984,'Vj'],
+	[51.511042,-0.31211,'pawel']
+]
 m = {
 	initialize : function (lat,lng) {
         var mapOptions = {
@@ -15,6 +21,39 @@ m = {
 		
 		marker.setMap(map);
     },
+	drop_markers : function(loc){
+		var options = {
+			zoom:1,
+			center: new google.maps.LatLng(51.5171, 0.1062),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(document.getElementById("cnv"),
+            options);
+		m.setMarkers(map, loc);
+	},
+	setMarkers : function(map, loc){
+		var bounds = new google.maps.LatLngBounds();
+		for (var i=0; i<loc.length; i++){
+			var ppl = loc[i]
+			var myLatLng = new google.maps.LatLng(loc[i][0], loc[i][1]);
+			bounds.union(new google.maps.LatLngBounds(myLatLng));
+			var marker = new google.maps.Marker({
+				position:myLatLng,
+				map:map,
+				title:loc[i][2]
+			})
+		}
+		map.fitBounds(bounds);
+	},
+	zoomToViewport : function( locations, map ){
+		console.log(locations)
+		var bounds = new google.maps.LatLngBounds();
+
+		for (var i in locations) {
+			bounds.union(locations[i].geometry.viewport);
+		}
+		map.fitBounds(bounds);
+	},
 	cl : function(){
 		$('.over').click(function(e){
 			e.preventDefault();
@@ -32,6 +71,14 @@ m = {
 			setTimeout(function() {
 				$(ui.vid).appendTo('#cnv');
 			}, 300);
+		})
+		$('.map_all').click(function(e){
+			e.preventDefault();
+			$('#cnv').empty();
+			$('.modal').modal();
+			setTimeout(function() {
+				m.drop_markers(p);
+			}, 900);
 		})
 	}
 	
