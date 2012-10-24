5f018e500a2a56dc4f4effd470ef43b0460e76e0
tomekl <tomasz.lipowicz@gmail.com>
2012-10-24 11:59:44 +0100

    video test

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ m = {
 			zoom: 15,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
+        var map = new google.maps.Map(document.getElementById("cnv"),
             mapOptions);
 		var marker = new google.maps.Marker({
 			position: mapOptions.center,
@@ -18,15 +18,27 @@ m = {
 	cl : function(){
 		$('.over').click(function(e){
 			e.preventDefault();
-			$('#map_canvas').empty();
+			$('#cnv').empty();
 			var dt = $(this).data();
 			$('.modal').modal();
 			setTimeout(function() {
 				m.initialize(dt.coor[0], dt.coor[1]);
 			}, 300);	
+		});
+		$('.vid').click(function(e){
+			e.preventDefault();
+			$('#cnv').empty();
+			$('.modal').modal();
+			setTimeout(function() {
+				$(ui.vid).appendTo('#cnv');
+			}, 300);
 		})
 	}
+	
 	  
+}
+ui = {
+	vid : '<iframe width="500" height="285" src="http://www.youtube.com/embed/40F2gslPhvo?rel=0" frameborder="0" allowfullscreen></iframe>'
 }
 $(document).ready(function(){
 	m.cl();
